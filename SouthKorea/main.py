805e8e0632cf675e2872e2d579e4a1390ca03764
David <nakdt9@gmail.com>
2025-04-18 22:37:37 +0200

    dataset description

--- SouthKorea/main.py
+++ SouthKorea/main.py
@@ -1,1 +1,8 @@
-print("test")
+import pandas as pd
+
+df = pd.read_csv('seoul_real_estate.csv', encoding='euc-kr')
+print(df.head())
+print(df.info())
+print(df.describe())
+
+print(df.isnull().sum())
