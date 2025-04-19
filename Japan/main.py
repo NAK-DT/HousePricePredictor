c2206b679b25e87c99ba6735d2c26fd827d2fd91
David <nakdt9@gmail.com>
2025-04-19 19:48:04 +0200

    added a japanese real estate dataset, imports

--- Japan/main.py
+++ Japan/main.py
@@ -1,1 +1,19 @@
-print("test1")
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+import tqdm
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.model_selection import train_test_split 
+from sklearn import metrics 
+from sklearn.preprocessing import OneHotEncoder
+import sklearn
+import gc
+import sys
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from copy import deepcopy
+from sklearn.metrics import r2_score
+import itertools
+import matplotlib.pyplot as plt
+import os
--- readme.md
+++ readme.md
@@ -11,3 +11,7 @@ https://www.kaggle.com/datasets/jcy1996/seoul-real-estate-datasets
 Free but requires login
 https://data.seoul.go.kr/dataList/OA-21275/S/1/datasetView.do
 
+
+
+Japan Real Estate 1 GB
+https://www.kaggle.com/datasets/nishiodens/japan-real-estate-transaction-prices
